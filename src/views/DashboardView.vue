017df4df067979470c4a2fe841711547611c9a51
mihakonceptcorn <skinzzadmin@gmail.com>
2025-05-31 16:05:52 +0300

    dashboard calendar

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,7 @@
 <template>
   <Card>
     <template #content>
-      <FullCalendar :options='calendarOptions' />
+      <FullCalendar :options='calendarOptions'></FullCalendar>
     </template>
   </Card>
 </template>
@@ -9,16 +9,30 @@
 <script setup>
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import { reactive } from 'vue'
+import { computed, reactive } from 'vue';
+import { useCompetitionsStore } from '@/stores/competitions.js'
+
+const competitionsStore = useCompetitionsStore()
+
+const events = computed(() => {
+  const events = []
+
+  competitionsStore.competitions.forEach((competition) => {
+    events.push({
+      title: competition.name,
+      start: new Date(competition.date),
+    })
+  })
+
+  return events
+})
 
 const calendarOptions = reactive(
   {
     plugins: [dayGridPlugin],
     initialView: 'dayGridMonth',
     weekends: true,
-    events: [
-      { title: 'Meeting', start: new Date() }
-    ]
+    events: events
   }
 )
 
