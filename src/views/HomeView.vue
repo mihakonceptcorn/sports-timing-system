92eebd8f2d21fe6ef19538d844ba124706c7a4b3
mihakonceptcorn <skinzzadmin@gmail.com>
2025-07-24 20:24:42 +0300

    title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
       <template #content>
         <div class="bg-surface-0 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20">
           <div class="text-surface-700 dark:text-surface-100 text-center flex flex-col items-center gap-4">
-            <div class="text-primary font-bold text-lg leading-tight">
+            <div class="text-primary font-bold text-3xl leading-tight">
               <h1>Sports Timing System</h1>
             </div>
             <div class="text-surface-900 dark:text-surface-0 font-bold text-4xl leading-tight">
